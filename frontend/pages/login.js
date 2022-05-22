7812011ff60ea13333bdefbc9a490581b1cc6094
polycloud-dev <heidenreichdd@gmail.com>
2022-05-22 22:18:24 +0200

    created example login

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,15 +1,27 @@
+import { Text, Button, Title, Space, Code, Anchor } from "@mantine/core"
 import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Component() {
     const { data: session } = useSession()
     if (session) {
-        return <>
-            Signed in as {session.user.email} <br />
-            <button onClick={() => signOut()}>Sign out</button>
-        </>
+        return (
+            <Text align="center">
+                <Title mt={100}>Signed in</Title>
+                <Space h='md' />
+                <Button onClick={signOut}>Sign out</Button>
+            </Text>
+        )
     }
-    return <>
-        Not signed in <br />
-        <button onClick={() => signIn()}>Sign in</button>
-    </>
+    return (
+        <Text align="center">
+            <Space h={100} />
+            <Title>Not signed in</Title>
+            <Space h='md' />
+            <Button onClick={signIn}>Sign in</Button>
+            <Space h='xl' />
+            Get started by setting up providers in <Code>{'/pages/api/[...nextauth].js'}</Code>
+            <br />
+            Read more about <Anchor href="https://next-auth.js.org">NextAuth.js</Anchor>
+        </Text>
+    )
 }
