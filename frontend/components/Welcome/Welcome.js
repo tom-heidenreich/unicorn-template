13ffbbe8bfb8968461cf9967da7bbe37e73ac7b1
polycloud-dev <heidenreichdd@gmail.com>
2022-05-22 22:17:20 +0200

    changed title

--- frontend/components/Welcome/Welcome.js
+++ frontend/components/Welcome/Welcome.js
@@ -1,15 +1,15 @@
-import { Title, Text, Anchor } from '@mantine/core';
+import { Title, Text, Anchor, Space } from '@mantine/core';
 import useStyles from './Welcome.styles';
 
 export function Welcome() {
   const { classes } = useStyles();
 
   return (
-    <>
+    <Text style={{'userSelect': 'none'}}>
       <Title className={classes.title} align="center" mt={100}>
-        Welcome to{' '}
-        <Text inherit variant="gradient" component="span">
-          Mantine
+        Welcome to the <br/> next {' '}
+        <Text inherit variant="gradient" component="span" gradient={{ from: 'mediumvioletred', to: 'hotpink', deg: 45 }}>
+          Unicorn
         </Text>
       </Title>
       <Text color="dimmed" align="center" size="lg" sx={{ maxWidth: 580 }} mx="auto" mt="xl">
@@ -20,6 +20,17 @@ export function Welcome() {
         </Anchor>
         . To get started edit index.js file.
       </Text>
-    </>
+      <Space mt="xl" />
+      <Title align="center" size="lg" sx={{ maxWidth: 580 }} mx="auto" mt="xl">
+        What modules are used?
+      </Title>
+      <Text color="dimmed" align="center" size="lg" sx={{ maxWidth: 580 }} mx="auto" mt="xl">
+        This starter Next.js project includes modules such as Mantine, NextAuth.js and {' '}
+        <Anchor href="/modules">
+         more
+        </Anchor>
+        .
+      </Text>
+    </Text>
   );
 }
