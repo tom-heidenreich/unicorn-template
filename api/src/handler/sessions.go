b1510a75a093f1bbbacdd1b95237a9de204e2d6a
polycloud-dev <heidenreichdd@gmail.com>
2022-05-23 21:44:43 +0200

    sessions handler

--- api/src/handler/sessions.go
+++ api/src/handler/sessions.go
@@ -1,11 +1,26 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"api/client"
 
-
+	"github.com/gin-gonic/gin"
+)
 
 var SessionHandler gin.HandlerFunc = func(ctx *gin.Context) {
+
+	sessionId := ctx.Param("sessionId")
+
+	session, err := client.GetSession(sessionId)
+
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
 	ctx.JSON(200, gin.H{
-		"message": "cannot get sessions",
+		"username": session.Username,
+		"password": session.Password,
 	})
 }
