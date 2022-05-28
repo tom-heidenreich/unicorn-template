8213701c01a97e778ced8dc8b901ee13414862da
polycloud-dev <heidenreichdd@gmail.com>
2022-05-28 18:45:34 +0200

    user handler works now with mongodb

--- api/src/handler/user.go
+++ api/src/handler/user.go
@@ -1,25 +1,70 @@
 package handler
 
 import (
+	"api/client/mongodb"
+	"api/vendor/go.mongodb.org/mongo-driver/bson/primitive"
+	"encoding/json"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func UserGetHandler(ctx *gin.Context) {
-}
-
-type UserForm struct {
+type User struct {
 	User     string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 	Email    string `json:"email" binding:"required"`
 }
 
+func UserGetHandler(ctx *gin.Context) {
+
+	id := ctx.Param("id")
+	objectId, _ := primitive.ObjectIDFromHex(id)
+
+	// find user by id
+	var result User
+	err := mongodb.Database.Collection("users").FindOne(ctx, bson.M{"_id": objectId}).Decode(&result)
+
+	if err == mongo.ErrNoDocuments {
+
+		ctx.JSON(404, gin.H{"error": "user not found"})
+		return
+
+	} else if err != nil {
+
+		println(err.Error())
+		ctx.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	json, err := json.Marshal(result)
+
+	if err != nil {
+		ctx.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(200, string(json))
+}
+
 func UserPostHandler(ctx *gin.Context) {
 
-	var json UserForm
+	var json User
 	if err := ctx.ShouldBindJSON(&json); err != nil {
 		println(err.Error())
 		ctx.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
+
+	// insert user
+	res, err := mongodb.Database.Collection("users").InsertOne(ctx, json)
+
+	if err != nil {
+		println(err.Error())
+		ctx.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	id := res.InsertedID
+
+	ctx.JSON(200, gin.H{"id": id})
 }
